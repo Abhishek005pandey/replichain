b9293d420e455292a9f453d7ee1e9a265621a91a
Abhishek005pandey <abhishek.pandey@adypu.edu.in>
2025-11-12 23:16:12 +0530

    Test: simulate backend failure to trigger rollback

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-  res.send('✅ Backend running successfully!');
+  res.send(✅ Backend running successfully!);
 });
 
 app.listen(PORT, () => {
